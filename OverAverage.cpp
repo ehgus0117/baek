bb365ebc2a5f9b73647a8ae8f6e8350b5c2c5133
ehgus0117 <fermas0372@naver.com>
2023-03-27 17:37:34 +0900

    0327

--- OverAverage.cpp
+++ OverAverage.cpp
@@ -6,7 +6,37 @@
 //
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-
+int main(void)
+{
+    int c, n, sum, cnt, i, j;
+    float avg, ratio;
+    int score[1000];
+    
+    cin>>c;
+    for(i=0; i<c; i++)
+    {
+        sum =0;
+        cnt =0;
+        cin>>n;
+        for(j=0; j<n; j++)
+        {
+            cin>>score[j];
+            sum+=score[j];
+        }
+        avg = (float)sum/n;
+        
+        for(j=0; j<n; j++)
+        {
+            if(score[j]>avg)
+                cnt++;
+        }
+        
+        ratio = (float)cnt/n*100;
+        cout<<fixed<<setprecision(3)<<ratio<<'%'<<endl;
+    }
+    return 0;
+}
